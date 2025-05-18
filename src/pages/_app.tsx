786ea6c06a5c4d7b06b8bed7cf7f1329e99bb36e
Jeong DongWon <chorok446@gmail.com>
2025-05-19 04:08:03 +0900

    app 수정정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 import { ScheduleProvider } from '../contexts/ScheduleContext';
+import { DutyProvider } from '../contexts/DutyContext'; // 추가
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ScheduleProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <DutyProvider> {/* DutyProvider로 감싸기 */}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </DutyProvider>
     </ScheduleProvider>
   );
 }
